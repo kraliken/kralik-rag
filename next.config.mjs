9c55cfcddce7a4abed6e8303fbb6c74da9de7e89
Kralik N <kraaliknorbert@gmail.com>
2026-01-24 21:12:59 +0100

    change node-version: "24.x"

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   distDir: 'build',
   output: 'standalone',
-  experimental: {
-    // Ez oldja meg a MongoDB SyntaxError-t az Azure-ön
-    serverComponentsExternalPackages: ['mongodb'],
-  },
   env: {
     CUSTOM_API_URL: process.env.CUSTOM_API_URL,
   },
